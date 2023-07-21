fe5b289b58352326e7fff3f2968a7d8bad3e4b9f
João Henrique De Oliveira <joaohenri293@gmail.com>
2023-07-21 14:14:07 -0300

    AC Para apagar e não permitindo mais de uma virgula

--- src/br/com/joaohenriDev/calc/modelo/Memoria.java
+++ src/br/com/joaohenriDev/calc/modelo/Memoria.java
@@ -1,10 +1,22 @@
 package br.com.joaohenriDev.calc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memoria {
+
+    private enum TipoComando {
+            ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA
+    };
 
     private static final Memoria instancia = new Memoria();
 
+    private final List<MemoriaObservador> observadores = new ArrayList<>();
+
+    private TipoComando ultimoOperacao = null;
+    private boolean substituir = false;
     private String textoAtual = "";
+    private String textoBuffer = "";
     private Memoria() {
 
     }
@@ -13,7 +25,61 @@ public class Memoria {
         return instancia;
     }
 
+    public void adicionarObservador(MemoriaObservador observador) {
+        observadores.add(observador);
+    }
     public String getTextoAtual() {
         return textoAtual.isEmpty() ? "0" : textoAtual;
+    }
+
+    public void processarComando(String texto) {
+
+        TipoComando tipoComando = detectarTipoComando(texto);
+        System.out.println(tipoComando);
+
+        if (tipoComando == null) {
+            return;
+        } else if (tipoComando == TipoComando.ZERAR) {
+            textoAtual = "";
+            textoBuffer = "";
+            substituir = false;
+            ultimoOperacao = null;
+        } else if (tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
+            textoAtual = substituir ? texto : textoAtual + texto;
+            substituir = false;
+        } else {
+            
+        }
+
+        observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+    }
+
+    private TipoComando detectarTipoComando(String texto) {
+        if (textoAtual.isEmpty() && texto == "0") {
+            return null;
+        }
+
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        } catch (NumberFormatException e) {
+            // Quando não for número...
+            if ("AC".equals(texto)) {
+                return TipoComando.ZERAR;
+            } else if ("/".equals(texto)) {
+                return TipoComando.DIV;
+            } else if ("*".equals(texto)) {
+                return TipoComando.MULT;
+            } else if ("+".equals(texto)) {
+                return TipoComando.SOMA;
+            } else if ("-".equals(texto)) {
+                return TipoComando.SUB;
+            } else if ("=".equals(texto)) {
+                return TipoComando.IGUAL;
+            } else if (",".equals(texto) && !textoAtual.contains(",")) {
+                return TipoComando.VIRGULA;
+            }
+        }
+        return null;
     }
 }
--- src/br/com/joaohenriDev/calc/visao/Display.java
+++ src/br/com/joaohenriDev/calc/visao/Display.java
@@ -1,15 +1,18 @@
 package br.com.joaohenriDev.calc.visao;
 
 import br.com.joaohenriDev.calc.modelo.Memoria;
+import br.com.joaohenriDev.calc.modelo.MemoriaObservador;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.*;
 
-public class Display extends JPanel {
+public class Display extends JPanel implements MemoriaObservador {
 
     private final JLabel label;
     public Display() {
+        Memoria.getInstancia().adicionarObservador(this);
+
         setBackground(new Color(46,49,50));
         label = new JLabel(Memoria.getInstancia().getTextoAtual());
         label.setForeground(Color.WHITE);
@@ -18,5 +21,10 @@ public class Display extends JPanel {
         setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
 
         add(label);
+    }
+
+    @Override
+    public void valorAlterado(String novoValor) {
+        label.setText(novoValor);
     }
 }
